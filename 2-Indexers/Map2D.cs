4396db84894431548b09eebc011a580248de1ebc
Andrea Foschi <andrea.foschi13@studio.unibo.it>
2020-12-14 12:15:12 +0100

    Completed Indexers

--- 2-Indexers/Map2D.cs
+++ 2-Indexers/Map2D.cs
@@ -8,72 +8,98 @@ namespace Indexers
     /// <inheritdoc cref="IMap2D{TKey1,TKey2,TValue}" />
     public class Map2D<TKey1, TKey2, TValue> : IMap2D<TKey1, TKey2, TValue>
     {
+        private readonly Dictionary<Tuple<TKey1, TKey2>, TValue> data = new Dictionary<Tuple<TKey1, TKey2>, TValue>();
         /// <inheritdoc cref="IMap2D{TKey1, TKey2, TValue}.NumberOfElements" />
-        public int NumberOfElements
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-        }
+        public int NumberOfElements => this.data.Count;
 
         /// <inheritdoc cref="IMap2D{TKey1, TKey2, TValue}.this" />
         public TValue this[TKey1 key1, TKey2 key2]
         {
-            get { throw new NotImplementedException(); }
-            set { throw new NotImplementedException(); }
+            get { return this.data[Tuple.Create(key1, key2)]; }
+            set { this.data[Tuple.Create(key1, key2)] = value; }
         }
 
         /// <inheritdoc cref="IMap2D{TKey1, TKey2, TValue}.GetRow(TKey1)" />
         public IList<Tuple<TKey2, TValue>> GetRow(TKey1 key1)
         {
-            throw new NotImplementedException();
+            return this.data.Keys
+                .Where(tuple => tuple.Item1.Equals(key1))
+                .Select(tuple => Tuple.Create(tuple.Item2, this.data[tuple]))
+                .ToList();
         }
 
         /// <inheritdoc cref="IMap2D{TKey1, TKey2, TValue}.GetColumn(TKey2)" />
         public IList<Tuple<TKey1, TValue>> GetColumn(TKey2 key2)
         {
-            throw new NotImplementedException();
+            return this.data.Keys
+                .Where(tuple => tuple.Item2.Equals(key2))
+                .Select(tuple => Tuple.Create(tuple.Item1, this.data[tuple]))
+                .ToList();
         }
 
         /// <inheritdoc cref="IMap2D{TKey1, TKey2, TValue}.GetElements" />
         public IList<Tuple<TKey1, TKey2, TValue>> GetElements()
         {
-            throw new NotImplementedException();
+            return this.data.Keys
+                .Select(tuple => Tuple.Create(tuple.Item1, tuple.Item2, this.data[tuple]))
+                .ToList();
         }
 
         /// <inheritdoc cref="IMap2D{TKey1, TKey2, TValue}.Fill(IEnumerable{TKey1}, IEnumerable{TKey2}, Func{TKey1, TKey2, TValue})" />
         public void Fill(IEnumerable<TKey1> keys1, IEnumerable<TKey2> keys2, Func<TKey1, TKey2, TValue> generator)
         {
-            throw new NotImplementedException();
+            var arrayKeys = keys2.ToArray();
+
+            foreach (var key1 in keys1)
+            {
+                foreach (var key2 in arrayKeys)
+                {
+                    this[key1, key2] = generator(key1, key2);
+                }
+            }
         }
 
         /// <inheritdoc cref="IEquatable{T}.Equals(T)" />
+        public bool Equals(Map2D<TKey1, TKey2, TValue> other)
+        {
+            return Equals(this.data, other.data);
+        }
+
+        /// <inheritdoc cref="IEquatable{T}.Equals(T)"/>
         public bool Equals(IMap2D<TKey1, TKey2, TValue> other)
         {
-            // TODO: improve
-            return base.Equals(other);
+            return other is Map2D<TKey1, TKey2, TValue> otherMap ? this.Equals(otherMap) : false;
         }
-
-        /// <inheritdoc cref="object.Equals(object?)" />
         public override bool Equals(object obj)
         {
-            // TODO: improve
-            return base.Equals(obj);
-        }
+            if (obj is null)
+            {
+                return false;
+            }
 
+            if (obj == this)
+            {
+                return true;
+            }
+
+            if (obj.GetType() != this.GetType())
+            {
+                return false;
+            }
+
+            return this.Equals(obj as Map2D<TKey1, TKey2, TValue>);
+        }
         /// <inheritdoc cref="object.GetHashCode"/>
         public override int GetHashCode()
         {
-            // TODO: improve
-            return base.GetHashCode();
+            return this.data != null ? this.data.GetHashCode() : 0;
         }
 
         /// <inheritdoc cref="IMap2D{TKey1, TKey2, TValue}.ToString"/>
         public override string ToString()
         {
-            // TODO: improve
-            return base.ToString();
+            return "{" + string.Join(", ", this.GetElements()
+                .Select(e => $"({e.Item1}, {e.Item2}, {e.Item3}")) + "}";
         }
     }
 }
